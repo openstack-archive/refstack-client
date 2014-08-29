df4f1e9f25276e265d9f8cde9aaec3ce53f8d9c9
Catherine Diep <cdiep@us.ibm.com>
2014-08-29 12:48:32 -0700

    Remove unused packages.
    Remove unused packages in setup_ubuntu_env.sh.
    
    Change-Id: Id199e286db23106b42ee5f7eeaebbecef98bcf56

--- setup_ubuntu_env.sh
+++ setup_ubuntu_env.sh
@@ -1,8 +1,7 @@
 # insure base requirements are installed
 sudo apt-get install -y git python-pip wget unzip
 sudo apt-get install -y libxml2-dev libxslt-dev lib32z1-dev python2.7-dev libssl-dev
-sudo apt-get install -y python-dev libxslt1-dev libsasl2-dev libsqlite3-dev libldap2-dev libffi-dev
-sudo apt-get install libxml2-python
+sudo apt-get install -y libsasl2-dev libffi-dev
 sudo pip install virtualenv
 
 # If we've already created it, source it. If not, start and then source it.
