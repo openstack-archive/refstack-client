e950efdc7342c5646d5ec3224fd1913f70bf7e2d
Chandan Kumar <chkumar@redhat.com>
2017-07-25 12:11:58 +0530

    Switch to refstack-client binary and some cleanup
    * Use latest pbr >= 2.0.0
    * Removed gitpython as it is not used within the code
    * switched to refstack-client binary in README.rst
    * Removed unwanted stuff from setup.py as those are already in setup.cfg
    * use pip install -e . to auto install dependencies from requirements.txt
      as well as package.
    
    Change-Id: Ia0b1b0a598c48200dc91cc08af9bd78d5685091e

--- setup.py
+++ setup.py
@@ -16,9 +16,14 @@
 
 import setuptools
 
+# In python < 2.7.4, a lazy loading of package `pbr` will break
+# setuptools if some other modules registered functions in `atexit`.
+# solution from: http://bugs.python.org/issue15881#msg170215
+try:
+    import multiprocessing  # noqa
+except ImportError:
+    pass
+
 setuptools.setup(
-    setup_requires=['pbr'],
-    version=0.1,
-    author='David Lenwell',
-    description='A testing tool used primarily by refstack.',
+    setup_requires=['pbr>=2.0.0'],
     pbr=True)
